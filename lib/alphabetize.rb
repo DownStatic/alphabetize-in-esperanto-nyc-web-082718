e50aba8c844dab4c0ef86ab10640ec6b9a7ebf76
Christopher Stull <chris.stull@gmail.com>
2018-08-17 20:00:50 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,11 @@
 def alphabetize(arr)
  ESPERANTO_ALPHABET = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ 
+ index = 0
+ ea_hash = {}
+ while index<ESPERANTO_ALPHABET.length do
+   ea_hash[index] = ESPERANTO_ALPHABET[index]
+   index = index + 1
+ end
+ 
 end
