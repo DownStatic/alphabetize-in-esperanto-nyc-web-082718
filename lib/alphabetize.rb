890c47a8a63326db0a31dd56fa8b26ed3569e311
Christopher Stull <chris.stull@gmail.com>
2018-08-17 20:09:43 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,8 +1,10 @@
 def alphabetize(arr)
  
+esperanto_alphabet = 
+ 
 arr.sort_by do |sentence|
     sentence.split("").map do |character|
-      ESPERANTO_ALPHABET.index(character)
+      esperanto_alphabet.index(character)
     end
   end
 end 
